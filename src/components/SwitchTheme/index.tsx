2546b2006da65baaff201cbdfe47f5a14481b23e
rwietter <rwietter@zohomail.com>
2024-06-20 22:35:25 -0300

    fix: hydration error because there was button inside another button

--- src/components/SwitchTheme/index.tsx
+++ src/components/SwitchTheme/index.tsx
@@ -1,57 +1,58 @@
-'use client'
+"use client";
 
-import { FC, useEffect, useRef } from 'react'
-import { MdSunny } from 'react-icons/md'
-import styles from './styles.module.css'
+import { FC, useEffect, useRef } from "react";
+import { MdSunny } from "react-icons/md";
+import styles from "./styles.module.css";
 
 interface Props {
-  visible: 'header' | 'sticky'
+	visible: "header" | "sticky";
 }
 
 const updateTheme = () => {
-  const theme = localStorage.getItem('theme') || ''
-  const html = document.querySelector('html')
-  if (!html) return
-  html.classList.value = theme
-  return
-}
+	const theme = localStorage.getItem("theme") || "";
+	const html = document.querySelector("html");
+	if (!html) return;
+	html.classList.value = theme;
+	return;
+};
 
 const storeTheme = () => {
-  const theme = document.querySelector('html')?.classList?.value || ''
-  localStorage.setItem('theme', theme)
-  return
-}
+	const theme = document.querySelector("html")?.classList?.value || "";
+	localStorage.setItem("theme", theme);
+	return;
+};
 
 const SwitchTheme: FC<Props> = ({ visible }) => {
-  const buttonRef = useRef<HTMLButtonElement>(null)
+	const buttonRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    updateTheme()
-  }, [])
+	useEffect(() => {
+		updateTheme();
+	}, []);
 
-  const handleSwitchTheme = () => {
-    buttonRef.current?.setAttribute('data-active', 'true')
-    document.querySelector('html')?.classList.toggle('dark')
-    storeTheme()
+	const handleSwitchTheme = () => {
+		buttonRef.current?.setAttribute("data-active", "true");
+		document.querySelector("html")?.classList.toggle("dark");
+		storeTheme();
 
-    const timeout = setTimeout(() => {
-      buttonRef.current?.removeAttribute('data-active')
-      clearTimeout(timeout)
-    }, 500)
-    return
-  }
+		const timeout = setTimeout(() => {
+			buttonRef.current?.removeAttribute("data-active");
+			clearTimeout(timeout);
+		}, 500);
+		return;
+	};
 
-  return (
-    <button
-      className={`${styles.theme} theme`}
-      type='button'
-      onClick={handleSwitchTheme}
-      ref={buttonRef}
-      data-visible={visible}
-    >
-      <MdSunny aria-hidden='true' />
-    </button>
-  )
-}
+	return (
+		// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+		<div
+			className={`${styles.theme} theme`}
+			onClick={handleSwitchTheme}
+			ref={buttonRef}
+			aria-hidden="true"
+			data-visible={visible}
+		>
+			<MdSunny />
+		</div>
+	);
+};
 
-export default SwitchTheme
+export default SwitchTheme;
