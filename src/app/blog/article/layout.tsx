102a8c972fec7efdd99ad0f485299a46ce86125f
rwietter <rwietter@zohomail.com>
2024-06-20 21:20:52 -0300

    remove unused article fonts

--- src/app/blog/article/layout.tsx
+++ src/app/blog/article/layout.tsx
@@ -1,12 +1,11 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren } from "react";
 
-import '../../../../styles/dracula-prism.css'
-import '../../../../styles/article-fonts.css'
+import "../../../../styles/dracula-prism.css";
 
-type Props = PropsWithChildren
+type Props = PropsWithChildren;
 
 const ArticleLayout: FC<Props> = ({ children }) => {
-  return <div className='fade-in-layout'>{children}</div>
-}
+	return <div className="fade-in-layout">{children}</div>;
+};
 
-export default ArticleLayout
+export default ArticleLayout;
