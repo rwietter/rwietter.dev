cbc1b858cde10bdccd9878b29dc424bda5b8dea4
rwietter <rwietter@zohomail.com>
2024-08-22 23:36:20 -0300

    add revalidate on fetch data

--- services/fetcherWeather.ts
+++ services/fetcherWeather.ts
@@ -1,2 +1,7 @@
-const API_WEATHER = `https://dataservice.accuweather.com/currentconditions/v1/${process.env.ACCUWEATHER_CITY_ID}?apikey=${process.env.ACCUWEATHER_API_KEY}`
-export const fetcherWeather = () => fetch(API_WEATHER).then((res) => res.json())
+const API_WEATHER = `https://dataservice.accuweather.com/currentconditions/v1/${process.env.ACCUWEATHER_CITY_ID}?apikey=${process.env.ACCUWEATHER_API_KEY}`;
+
+export const fetcherWeather = () =>
+	fetch(API_WEATHER, {
+		next: { revalidate: 300 },
+		cache: "force-cache",
+	}).then((res) => res.json());
